917fbe8100415db1d9117e601ee4bac5e4e120d6
Rohan Singh <rohan.buildai@gmail.com>
2026-09-12 13:53:44 +0530

    feat: setup postgresSQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 
 const app = require("./app");
+const pool = require("./config/db") ;
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`server running on http://localhost:${PORT}`);
-});
+async function startServer()
+{
+    try{
+        await pool.query("SELECT NOW()");
+
+        console.log("✅ Database Connected");
+
+        app.listen(PORT , ()=>{
+            console.log(`server running on http://localhost:${PORT}`)
+        });
+    }
+    catch(err){
+        console.error("❌ database connection failed")
+        console.error(err.message);
+    }
+}
+
+startServer();
